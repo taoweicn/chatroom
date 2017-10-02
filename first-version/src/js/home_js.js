e735cddbd9c8b84080d71f9293c2795de577920c
TaoWei <twchn@live.com>
2017-10-03 00:07:31 +0800

    Finish chatroom static page

---
+++ first-version/src/js/home_js.js
@@ -0,0 +1,8 @@
+/*绑定新建弹出层*/
+$(".create-room").on("click", function () {
+	$("body").css("overflow", "hidden");
+	$(".mask").fadeIn().find(".close-btn").on("click", function () {
+		$(".mask").fadeOut();
+		$("body").css("overflow", "auto");
+	});
+});
